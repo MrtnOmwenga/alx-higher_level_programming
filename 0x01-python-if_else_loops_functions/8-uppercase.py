20e87fe55f54b7d86f478e66f071f6004c8fcb96
MrtnOmwenga <martin36449@gmail.com>
2022-11-29 19:23:08 -0800

    Uppercase function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result =''.join([chr(ord(char) - 32) for char in str if ord(char) >= 65])
+    result = ''
+    for char in str:
+        if ord(char) >= 65:
+            result = result.join(chr(ord(char) - 32))
+        else:
+            result = result.join(char)
     print("{result}".format(result=result))
