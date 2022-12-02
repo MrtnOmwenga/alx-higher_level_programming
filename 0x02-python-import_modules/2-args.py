cc65f47a7f1bdeebcdd4fa44d54bb14fdfd685ba
MrtnOmwenga <martin36449@gmail.com>
2022-12-01 18:52:57 -0800

    Debugging

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,11 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv)
     if count == 1:
-        print("{:d} argument.".format(count))
+        print("{:d} arguments.".format(0))
     else:
-        print("{:d} arguments:".format(count))
+        if count == 2:
+            print("1 argument:")
+        else:
+            print("{:d} arguments:".format(count - 1))
         for i in range(1, count):
             print("{:d}: {}".format(i, sys.argv[i]))
