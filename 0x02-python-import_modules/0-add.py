d748b403406b3aa7758c3f57166c3787699841c5
MrtnOmwenga <martin36449@gmail.com>
2022-12-01 18:22:52 -0800

    Restructured import line

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0 as add
 a = 1
 b = 2
-print("{a:d} + {b:d} = {num:d}".format(a=a, b=b, num=add(a, b)))
+print("{a:d} + {b:d} = {num:d}".format(a=a, b=b, num=add.add(a, b)))
