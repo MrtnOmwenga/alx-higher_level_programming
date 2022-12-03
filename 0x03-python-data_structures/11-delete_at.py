87d0c3e1c464d4bbbdb4202af299742ac4eb2268
MrtnOmwenga <martin36449@gmail.com>
2022-12-03 07:56:41 -0800

    Pycodestyle

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,5 @@
 def delete_at(my_list=[], idx=0):
     result = my_list
     if idx < len(my_list) and idx >= 0:
-        #result = my_list[:idx] + my_list[idx + 1:]
         del result[idx]
     return (result)
