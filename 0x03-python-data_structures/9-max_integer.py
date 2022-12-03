bc28942b44c54e33e6c2e2a2b0bb409bb86388d9
MrtnOmwenga <martin36449@gmail.com>
2022-12-03 07:15:52 -0800

    Handling negatives

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if my_list == []:
         return (None)
     else:
-        result = 0
+        result = float('-inf')
         for i in my_list:
             if i > result:
                 result = i
