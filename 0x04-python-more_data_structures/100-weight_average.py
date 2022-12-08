7f82020ecbebb7f2bb1e7520367070e5bea8eb10
MrtnOmwenga <martin36449@gmail.com>
2022-12-07 18:09:54 -0800

    Handling null values

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     result = 0
     weight_sum = 0
+    if my_list == []:
+        return (0)
     for i in my_list:
         result = result + (i[0] * i[1])
         weight_sum = weight_sum + i[1]
