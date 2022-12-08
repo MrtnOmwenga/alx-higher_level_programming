8e08abdf080d347d583d95cd529ad5980cef9cf5
MrtnOmwenga <martin36449@gmail.com>
2022-12-07 18:31:05 -0800

    Complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new_dict = a_dictionary
+    for key, dict_value in list(a_dictionary.items()):
+        if value == dict_value:
+            new_dict.pop(key, 0)
+    return (a_dictionary)
+    
