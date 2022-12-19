359a0f738ce8fa9c28150db79e3f90f293a8cd93
MrtnOmwenga <martin36449@gmail.com>
2022-12-19 11:30:38 -0800

    Pycodestyle

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    print("Inside result: ", end = "")
+    print("Inside result: ", end="")
     try:
         result = (a / b)
         return (result)
