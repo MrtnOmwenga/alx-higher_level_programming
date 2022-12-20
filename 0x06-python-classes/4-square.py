9cffa3dff2e20fd9b69464f78c04a4c6af9ba4be
MrtnOmwenga <martin36449@gmail.com>
2022-12-20 07:01:00 -0800

    Debugging

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,7 +26,7 @@ class Square:
     def size(self, value):
         """Sets size"""
         if type(value) is not int:
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
