c41c808f5fea893ee9d042994e04ec495e8a11ae
MrtnOmwenga <martin36449@gmail.com>
2022-12-20 05:44:35 -0800

    Class Square with function area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Square with private instance attribute size"""
+
+
+class Square:
+    """Class Square with private instance attribute"""
+    def __init__(self, size=0):
+        """Initializes class"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    def area(self):
+        """Calculates area of square"""
+        return (self._Square__size * self._Square__size)
