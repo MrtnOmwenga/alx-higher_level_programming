c2ab96190e94dcc154432f44c87d02db485e6c2e
MrtnOmwenga <martin36449@gmail.com>
2022-12-20 06:56:49 -0800

    Property getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Class Square with private instance attribute size"""
+
+
+class Square:
+    """Class Square with private instance attribute"""
+    def __init__(self, size=0):
+        """Initializes class"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    def area(self):
+        """Calculates area of square"""
+        return (self._Square__size * self._Square__size)
+
+    @property
+    def size(self):
+        """Gets size"""
+        return (self._Square__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets size"""
+        if type(value) is not int:
+            raise TypeError("size mut be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = value
+        
