beda53e59e6efbbb5d69d3282e81a81795a8859d
MrtnOmwenga <martin36449@gmail.com>
2023-01-03 03:57:18 -0800

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+prints "My name is <first name> <last name>
+Both first and last names must be strings
+"""
+
+def say_my_name(first_name, last_name=""):
+    """Prints first name and last name"""
+
+    """Check first name"""
+    if first_name is None or type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    """Print statement"""
+    print("My name is {:s} {:s}".format(first_name, last_name))
