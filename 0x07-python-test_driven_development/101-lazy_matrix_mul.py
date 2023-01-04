2b62ef7170ba92dbf3459fb3cf164f4661e54746
MrtnOmwenga <martin36449@gmail.com>
2023-01-04 21:28:59 +0000

    Lazy matrix mul

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Multiplies matrix using NumPy."""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+        """Return the product of two matrices."""
+            return (np.matmul(m_a, m_b))
