2029e058fced15297be81f00777890d33aa7bb7b
MrtnOmwenga <martin36449@gmail.com>
2023-01-05 07:08:19 +0000

    Debugging

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Adds 2 integers / floats and returns integer
 Input must be integers or floats
@@ -7,9 +7,9 @@ Input must be integers or floats
 
 def add_integer(a, b=98):
     """add the 2 integers"""
-    if type(a) not in [int, float]:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if b is None or type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
     if a is None:
