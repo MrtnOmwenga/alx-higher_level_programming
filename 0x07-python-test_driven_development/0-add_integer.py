baf4fb5dd3cb769723ad82bd5c4e72264d4aadd6
MrtnOmwenga <martin36449@gmail.com>
2023-01-05 07:40:55 +0000

    Debugging

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,9 +7,9 @@ Input must be integers or floats
 
 def add_integer(a, b=98):
     """add the 2 integers"""
-    if a is None or type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) not in [int, float]:
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
         return (int(a) + int(b))
