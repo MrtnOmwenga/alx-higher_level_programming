71855d5ffec5539685b1cea17ac2f90999243828
MrtnOmwenga <martin36449@gmail.com>
2023-01-09 06:44:12 -0800

    isinstance function

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Checks if object is an
+instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """ Uses is instance function """
+
+    return (isinstance(obj, a_class))
