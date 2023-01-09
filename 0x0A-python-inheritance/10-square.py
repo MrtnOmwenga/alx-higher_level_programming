24df9974a864881fb427f5a126307b0371bfce67
MrtnOmwenga <martin36449@gmail.com>
2023-01-09 11:51:26 -0800

    Debugging

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Class square that inherits from Rectangle
+Private instance size
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Private instance attribute size"""
+
+    def __init__(self, size):
+        """ Initializes class """
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
