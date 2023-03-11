74415abe52dda09aafdb79671ddb57e1eed9c8c2
MrtnOmwenga <martin36449@gmail.com>
2023-03-11 10:17:53 -0800

    Convert class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,11 +4,8 @@ Converts class to json obj
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """
     Convert obj to jason
     """
-    return(json.dumps(obj.__dict__))
+    return(obj.__dict__)
