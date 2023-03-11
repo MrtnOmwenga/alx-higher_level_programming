733e4f440339b880e8f31643e69f3857959d1d0c
MrtnOmwenga <martin36449@gmail.com>
2023-03-11 08:42:53 -0800

    Convert to Json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Converts object to json
+Uses json
+"""
+
+import json
+def to_json_string(myobj):
+    """
+    Convert myobj to json
+    """
+    return(json.dumps(myobj))
