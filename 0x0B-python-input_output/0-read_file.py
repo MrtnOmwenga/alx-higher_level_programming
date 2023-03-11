912d608f3bc6b2d92b865d227b0fae19fecddc57
MrtnOmwenga <martin36449@gmail.com>
2023-03-11 08:22:18 -0800

    Debugging

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,10 @@ No file avilability checks
 
 
 def read_file(filename=""):
+    """
+    Open file and print the contents
+    Must use with
+    """
     with open(filename) as file:
         read_data = file.read()
         print(read_data)
