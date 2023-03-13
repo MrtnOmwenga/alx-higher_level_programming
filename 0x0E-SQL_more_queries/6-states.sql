9bc331f8616d9f9fd212db8929a6a5b66f4df0ee
MrtnOmwenga <martin36449@gmail.com>
2023-03-13 08:29:35 -0700

    Debugging

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- Create database
 -- Create table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `states` (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
