5b231471436641760e574366c6fa6ef369ebb581
MrtnOmwenga <martin36449@gmail.com>
2023-03-13 06:33:53 -0700

    Debugging

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create second table and fill it
-CREATE TABLE second_table (id INT, name VARCHAR(26), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(26), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
