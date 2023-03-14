31977943be601871ca078ee3fe6675248c00c4e2
MrtnOmwenga <martin36449@gmail.com>
2023-03-14 01:18:29 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!usr/bin/node
+//Include process module
+const process = require('process');
+
+const args = process.argv;
+if (args.length == 0){
+    console.log('No argument');
+}else if (args.length == 1){
+    console.log('Argument found');
+}else{
+    console.log('Arguments found');
+}
