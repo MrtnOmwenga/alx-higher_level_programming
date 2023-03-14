1fb5df621ea7d2de34b70462c2e065babb7aadd2
MrtnOmwenga <martin36449@gmail.com>
2023-03-14 01:29:03 +0000

    Debugging

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
-#!usr/bin/node
-
+#!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 2 ? : 'No Argument': count === 3 ? 'Argument found' : 'Arguments found');
+console.log(count === 2 ? 'No Argument': count === 3 ? 'Argument found' : 'Arguments found');
