e34bae5c728401701a048d7090f265757a9065d1
MrtnOmwenga <martin36449@gmail.com>
2023-03-19 14:25:59 -0700

    Model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+SQLAlchemy use
+"""
+
+
+from sqlalchemy import Integer, String, Column, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class City(Base):
+    """ Class city with id and name attributes """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
