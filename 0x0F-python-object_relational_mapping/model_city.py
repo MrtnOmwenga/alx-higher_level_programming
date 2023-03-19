f63f6ef08680b4576ef5dc04cfc0e1d3987040ce
MrtnOmwenga <martin36449@gmail.com>
2023-03-19 14:35:36 -0700

    Debugging

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,7 @@ SQLAlchemy use
 """
 
 
-from sqlalchemy import Integer, String, Column, MetaData
+from sqlalchemy import Integer, String, Column, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -16,4 +16,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
