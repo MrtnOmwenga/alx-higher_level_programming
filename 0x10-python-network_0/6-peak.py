3d5e28592a520afb7536d14ba98eb4021343be3c
MrtnOmwenga <martin36449@gmail.com>
2023-03-30 13:05:59 -0700

    Debugging

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,7 @@ Find Peak in unsirted list
 
 def find_peak(list_of_integers):
     """ Find Max """
-    if list_of_integers == [] or list_of_integers == None:
+    if list_of_integers == [] or list_of_integers is None:
         return None
 
     sort = sorted(list_of_integers)
