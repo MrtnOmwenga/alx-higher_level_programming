b303a2efc1dbf1ab86314e0f4684122eb39eeb7a
MrtnOmwenga <martin36449@gmail.com>
2023-03-30 11:58:06 -0700

    Debugging

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Show all methods a URL will allow
-curl -s -i -L -X OPTIONS $1 | awk -vOFS= '/Allow/ {$1=""; print $0}'
+curl -s -I -X OPTIONS $1 | awk -vOFS= '/Allow/ {$1=""; print $0}'
