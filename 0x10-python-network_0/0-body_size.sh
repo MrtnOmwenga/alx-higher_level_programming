31e197f6c4990eb40857f3bf14131d9214febba2
MrtnOmwenga <martin36449@gmail.com>
2023-03-30 08:47:13 -0700

    Debugging

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
 
-curl -X REQUEST $1 | grep -iF content-length
+curl -sI REQUEST $1 | grep -iF content-length
