9455f32fe19fcedfc9cbc8cc0e2b0e858907e0a3
MrtnOmwenga <martin36449@gmail.com>
2023-04-01 09:32:07 -0700

    DEcumentation

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,11 @@
 """ Return variable in header response """
 
 
-import urllib.request
-import sys
+if __name__ == '__main__':
+    import urllib.request
+    import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    data = response.headers['X-Request-Id']
-    print(data)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        data = response.headers['X-Request-Id']
+        print(data)
