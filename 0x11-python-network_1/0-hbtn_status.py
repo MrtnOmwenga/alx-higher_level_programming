65e7176520a22768ee8a4703d65dc84844b3328c
MrtnOmwenga <martin36449@gmail.com>
2023-04-01 09:17:13 -0700

    Debugging

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     print('Body response:')
-    print('\t-type: ' + str(type(response)))
-    print('\t-content: ' + str(response.read()))
-    print('\t-utf8 content: ' + str(response.read().decode('utf-8')))
+    print('\t- type: ' + str(type(response)))
+    print('\t- content: ' + str(response.read()))
+    print('\t- utf8 content: ' + str(response.read().decode('utf-8')))
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Return variable in header response """
+
+
+import urllib.request
+import sys
