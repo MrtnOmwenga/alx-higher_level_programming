e106eeacb7037df7a87ef8bda68b0d53f551aae5
MrtnOmwenga <martin36449@gmail.com>
2023-04-01 09:46:02 -0700

    POST request using urllib

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" POST request """
+
+
+if __name__ == '__main__':
+    from urllib import request, parse
+    import sys
+
+    email_dict = {'email': sys.argv[2]}
+    data = parse.urlencode(email_dict).encode()
+    req = request.Request(sys.argv[1], data=data)
+    response = request.urlopen(req)
+
+    print(response.read())
