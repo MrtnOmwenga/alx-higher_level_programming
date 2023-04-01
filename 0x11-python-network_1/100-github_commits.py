ac92ae2360079a333bc97da519dc0eda0d2a2e77
MrtnOmwenga <martin36449@gmail.com>
2023-04-01 12:31:32 -0700

    Solve Holberton Recruiting Problem

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Solve Recruiting Challenge """
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    import json
+
+    repo, owner = sys.argv[1:]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    headers = {'Accept': 'application/vnd.github+json'}
+    r = requests.get(url, headers=headers)
+    try:
+        res = json.loads(r.text)
+        for i in range(10):
+            sha = res[i]['sha']
+            name = res[i]['commit']['author']['name']
+            print('{}: {}'.format(sha, name))
+
+    except IndexError:
+        pass
